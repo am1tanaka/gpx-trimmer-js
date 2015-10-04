d0c3e2910fb0cd249280bbd7fca05591f8dad544
YuTanaka <YRK00337@nifty.com>
2015-10-04 20:57:03 +0900

    gulpとbrowserifyを組み込み

---
+++ src/libgpx.js
@@ -0,0 +1,12 @@
+/**
+ * GPXファイル操作のページ用のJavaScript
+ * @copyright 2015 YuTanaka
+ * @license MIT
+ */
+
+var gpxtrim = require('./gpx-trimmer');
+
+// パラメータ設定
+$('#fileGpx').change(function() {
+  alert('file');
+});
